8166b37ee70b790d3ef6b43eb514fa673b9cd91b
Vito Tardia <vito@akkroo.com>
2017-07-12 17:06:30 +0100

    Add event type constants

--- src/Event.php
+++ src/Event.php
@@ -3,4 +3,33 @@ namespace Akkroo;
 
 class Event extends Resource
 {
+    /**
+     * Data Capture
+     * @const
+     */
+    const TYPE_DATA_CAPTURE = 1;
+
+    /**
+     * Check-in (no Registration)
+     * @const
+     */
+    const TYPE_CHECKIN = 2;
+
+    /**
+     * Check-in or Register
+     * @const
+     */
+    const TYPE_CHECKIN_OR_REGISTER = 3;
+
+    /**
+     * Check-in then Data Capture (no Registration)
+     * @const
+     */
+    const TYPE_CHECKIN_THEN_DATA_CAPTURE = 4;
+
+    /**
+     * Check-in or Register then Data Capture
+     * @const
+     */
+    const TYPE_CHECKIN_OR_REGISTER_THEN_DATA_CAPTURE = 5;
 }
